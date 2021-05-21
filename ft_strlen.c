eb70eaac98ed4c45ebd1251e056ba937718ed8c5
Rodrigo Longhi <rfslonghi@gmail.com>
2021-05-21 19:00:03 -0300

    Tabs insted of spaces

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/21 00:57:10 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/05/21 02:55:19 by rfelipe-         ###   ########.fr       */
+/*   Created: 2021/05/21 18:15:56 by rfelipe-          #+#    #+#             */
+/*   Updated: 2021/05/21 18:15:56 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
+#include "libft.h"
 
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-
-#endif
+size_t	ft_strlen(const char *s)
+{
+	
+}
