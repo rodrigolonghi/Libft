c85ab6adbb0b7d56b7c697db7394f4459146059d
Rodrigo Longhi <rfslonghi@gmail.com>
2021-05-24 05:24:22 -0300

    ft_atoi added

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,12 +6,11 @@
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 04:14:50 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/05/24 05:13:56 by rfelipe-         ###   ########.fr       */
+/*   Updated: 2021/05/24 05:22:11 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_atoi(const char *nptr)
 {
