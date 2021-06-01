4f02c3d4d5888569890aeb52ac36ec9336df231e
Rodrigo Felipe dos santos longhi <rfelipe-@c1r5p2.42sp.org.br>
2021-06-01 01:23:10 -0300

    ft_memmove.c adjustments

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 01:38:45 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/05/27 22:38:36 by rfelipe-         ###   ########.fr       */
+/*   Updated: 2021/06/01 01:22:32 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t	i;
 	char	*dest;
 	char	*source;
 
-	i = 0;
 	dest = (char *)dst;
 	source = (char *)src;
 	if (dest > source)
